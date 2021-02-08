f50591291b579ba95ebaafd92563a8727b8218e2
Bektur <bakeevbektur@gmail.com>
2021-02-08 19:01:13 +0600

    add SecondActivity

--- app/src/main/java/kg/itrun/lessongit/MainActivity.java
+++ app/src/main/java/kg/itrun/lessongit/MainActivity.java
@@ -2,17 +2,27 @@ package kg.itrun.lessongit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button myButton;
+    Button second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myButton = findViewById(R.id.second_button);
+        second = findViewById(R.id.second_button);
+
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
