bd2a9c50789a962d218921406678f126388f35c2
Bektur <bakeevbektur@gmail.com>
2021-02-08 18:44:38 +0600

    add button

--- app/src/main/java/kg/itrun/lessongit/MainActivity.java
+++ app/src/main/java/kg/itrun/lessongit/MainActivity.java
@@ -3,15 +3,16 @@ package kg.itrun.lessongit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView myTextView;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myTextView = findViewById(R.id.hello_text_view);
+        myButton = findViewById(R.id.second_button);
     }
 }
